f4a59ed22c8c5679897124c866b393cdb599cb72
Aaron Brown <aaroncbrown1993@gmail.com>
2019-03-18 22:42:07 -0400

    changed math-problem-solver to calcuclump

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to math-problem-solver!');
+    expect(page.getTitleText()).toEqual('Welcome to calcuclump!');
   });
 
   afterEach(async () => {
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/math-problem-solver'),
+      dir: require('path').join(__dirname, '../coverage/calcuclump'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
